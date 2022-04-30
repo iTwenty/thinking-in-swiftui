e662029d857033447bf0bfdbfe6120ae97ac3a9f
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2022-04-30 16:06:55 +0530

    Initial commit

---
+++ 03_ImageLoader/03_ImageLoader/ImageLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageLoaderApp.swift
+//  03_ImageLoader
+//
+//  Created by Jaydeep Joshi on 30/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ImageLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
